1991f7fc4944150e2c7ae136f4306b433c4c580f
PaskoZhelev <pasko.zhelev@gmail.com>
2025-11-26 21:14:02 +0100

    fixes

--- run-app.sh
+++ run-app.sh
@@ -3,17 +3,19 @@
 # Stop script on first error
 set -e
 
-echo "🚀 Starting Simplified Deployment for Symbiomes..."
+echo "🚀 Starting Deployment for Symbiomes..."
 
 # 1. Pull latest changes (Keep this unchanged)
 echo "📥 Pulling from Git..."
 git fetch origin
 git reset --hard origin/master 
 
-echo "⏳ Waiting 5 seconds for container to stabilize..."
-sleep 5
+docker compose down
 
 docker-compose up -d --build
+
+echo "⏳ Waiting 5 seconds for container to stabilize..."
+sleep 5
 
 # 4. Restart host Nginx (Keep this unchanged)
 echo "🔄 Restarting host Nginx..."
