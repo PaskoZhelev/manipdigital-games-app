8ac143e5323bd8152e01306e50d9efc8133ae65b
PaskoZhelev <pasko.zhelev@gmail.com>
2025-11-26 20:45:34 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { SymbiomesGame } from './games/symbiomes/SymbiomesGame';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 
 function App() {
 
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/symbiomes" replace />} />
       <Route path="/symbiomes" element={<SymbiomesGame />} />
 
       {/* <Route path="*" element={<NotFound />} /> */}
