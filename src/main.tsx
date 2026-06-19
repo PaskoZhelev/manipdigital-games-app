f933fe676c4d57d6e816a996796abf6b233b4fd7
PaskoZhelev <pasko.zhelev@gmail.com>
2026-06-19 20:42:43 +0200

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
 createRoot(document.getElementById('root')!).render(
-<BrowserRouter basename={import.meta.env.BASE_URL}>
-  <App />
-</BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
 )
+
+// Navigate to the stored route after app loads
+if (redirect && redirect !== '/') {
+  window.history.replaceState(null, '', redirect);
+}
